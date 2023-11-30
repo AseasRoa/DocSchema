d941108a2f86b0343cce9064d1e37249fce6b47d
AseasRoa <aseasroa@proton.me>
2023-11-30 16:51:22 +0200

    Refactoring test files

--- tests/integration/DocSchema.test.js
+++ tests/integration/DocSchema.test.js
@@ -1,104 +1,108 @@
 import { docSchema, DocSchema, ValidationError } from '#docschema'
 
 describe('DocSchema', () => {
-  test('docSchema() and new DocSchema() are identical', () => {
-    /** @enum {string} */
-    const schema1 = docSchema()
+  describe('Schema Creation', () => {
+    test('docSchema() and new DocSchema() are identical', () => {
+      /** @enum {string} */
+      const schema1 = docSchema()
 
-    /** @enum {string} */
-    const schema2 = new DocSchema()
+      /** @enum {string} */
+      const schema2 = new DocSchema()
 
-    expect(schema1).toStrictEqual(schema2)
+      expect(schema1).toStrictEqual(schema2)
+    })
+
+    test('try to create schema, but without JsDoc comment above', () => {
+      expect(() => {
+        // No JsDoc comment at all
+
+        const schema = docSchema()
+      }).toThrow(SyntaxError)
+
+      expect(() => {
+        /**
+         * This JsDoc comment has an empty space below, which makes it invalid
+         *
+         * @enum {string}
+         */
+
+        const schema = docSchema()
+      }).toThrow(SyntaxError)
+
+      expect(() => {
+        /* eslint-disable-next-line jsdoc/no-bad-blocks */
+        /*
+         * This JsDoc comment is wrong, because it doesn't start with two stars
+         *
+         * @enum {string}
+         */
+        const schema = docSchema()
+      }).toThrow(SyntaxError)
+    })
   })
 
-  test('try to create schema, but without JsDoc comment above', () => {
-    expect(() => {
-      // No JsDoc comment at all
-
-      const schema = docSchema()
-    }).toThrow(SyntaxError)
-
-    expect(() => {
+  describe('approves(), validate()', () => {
+    test('correct validation', () => {
       /**
-       * This JsDoc comment has an empty space below, which makes it invalid
-       *
-       * @enum {string}
-       */
-
-      const schema = docSchema()
-    }).toThrow(SyntaxError)
-
-    expect(() => {
-      /* eslint-disable-next-line jsdoc/no-bad-blocks */
-      /*
-       * This JsDoc comment is wrong, because it doesn't start with two stars
-       *
-       * @enum {string}
+       * @enum {{
+       *   key1: string,
+       *   key2: number
+       * }}
        */
       const schema = docSchema()
-    }).toThrow(SyntaxError)
+
+      expect(schema.validate({ key1: '1', key2: 2})).toBe(true)
+      expect(schema.approves({ key1: '1', key2: 2})).toBe(true)
+    })
+
+    test('correct invalidation', () => {
+      /**
+       * @enum {{
+       *   key1: string,
+       *   key2: number
+       * }}
+       */
+      const schema = docSchema()
+
+      expect(() => schema.validate({ key1: 1, key2: '2'})).toThrow(ValidationError)
+      expect(schema.approves({ key1: 1, key2: '2'})).toBe(false)
+    })
+
+    test('correct validation with typedefs', () => {
+      /**
+       * @typedef {string} StringTypedef
+       */
+
+      /**
+       * @typedef {{ a:string, b: number }} ObjTypedef
+       */
+
+      /**
+       * @enum {{
+       *   key1: StringTypedef,
+       *   key2: ObjTypedef
+       * }}
+       */
+      const schema = docSchema()
+
+      expect(schema.validate({ key1: '1', key2: { a: '', b: 0 }})).toBe(true)
+      expect(schema.approves({ key1: '1', key2: { a: '', b: 0 }})).toBe(true)
+    })
+
+    test('correct invalidation with filters', () => {
+      /**
+       * @enum {{
+       *   key1: number, // {min: 3}
+       * }}
+       */
+      const schema = docSchema()
+
+      expect(() => schema.validate({ key1: 1})).toThrow(ValidationError)
+      expect(schema.approves({ key1: 1})).toBe(false)
+    })
   })
 
-  test('correct validation', () => {
-    /**
-     * @enum {{
-     *   key1: string,
-     *   key2: number
-     * }}
-     */
-    const schema = docSchema()
-
-    expect(schema.validate({ key1: '1', key2: 2})).toBe(true)
-    expect(schema.approves({ key1: '1', key2: 2})).toBe(true)
-  })
-
-  test('correct invalidation', () => {
-    /**
-     * @enum {{
-     *   key1: string,
-     *   key2: number
-     * }}
-     */
-    const schema = docSchema()
-
-    expect(() => schema.validate({ key1: 1, key2: '2'})).toThrow(ValidationError)
-    expect(schema.approves({ key1: 1, key2: '2'})).toBe(false)
-  })
-
-  test('correct validation with typedef', () => {
-    /**
-     * @typedef {string} StringTypedef
-     */
-
-    /**
-     * @typedef {{ a:string, b: number }} ObjTypedef
-     */
-
-    /**
-     * @enum {{
-     *   key1: StringTypedef,
-     *   key2: ObjTypedef
-     * }}
-     */
-    const schema = docSchema()
-
-    expect(schema.validate({ key1: '1', key2: { a: '', b: 0 }})).toBe(true)
-    expect(schema.approves({ key1: '1', key2: { a: '', b: 0 }})).toBe(true)
-  })
-
-  test('correct invalidation with filters', () => {
-    /**
-     * @enum {{
-     *   key1: number, // {min: 3}
-     * }}
-     */
-    const schema = docSchema()
-
-    expect(() => schema.validate({ key1: 1})).toThrow(ValidationError)
-    expect(schema.approves({ key1: 1})).toBe(false)
-  })
-
-  describe('check() function', () => {
+  describe('check()', () => {
     test('simple value', () => {
       /**
        * @enum {number} num { min: 5 }
