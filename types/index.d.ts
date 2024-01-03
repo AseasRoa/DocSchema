07eb46e64115fa61d0eef8fa68003959083b5787
AseasRoa <aseasroa@proton.me>
2024-01-03 15:35:08 +0200

    Refactoring types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,90 +1,14 @@
 /**
- * This file must be an ambient module
- *
- * @see https://www.typescriptlang.org/docs/handbook/modules/reference.html#ambient-modules
+ * This file contains types, exported and used by other modules.
  */
 
-type DocSchemaAst = import('../lib/types/app').Ast
-type DocSchemaCheckResult = import('../lib/types/app').CheckResult
+import { docSchema } from './types.d.ts'
 
-declare module 'docschema' {
-  export class ValidationError extends Error {
-    expectedType: string
-    filter: undefined | { name: '' | keyof Filters, value: boolean | number | string | RegExp }
-    kind: 'type' | 'filter' | ''
-    message: string
-    pass: boolean
-    tag: string
-    value: any
-    valuePath: PropertyKey[]
-  }
-
-  export class DocSchemaParser {
-    parseComments(
-      code: string,
-      file?: string
-    ): Promise<Ast[]>
-
-    parseFile(
-      file: string
-    ): Promise<Ast[]>
-
-    removeFileFromCache(
-      file: string
-    ): void
-  }
-
-  export class DocSchemaValidator {
-    validateFunctionArguments(
-      ast: Ast,
-      args: any,
-      throwOnError?: boolean
-    ): boolean
-
-    validateParams(
-      name: 'param' | 'property',
-      ast: Ast,
-      args: any,
-      throwOnError?: boolean
-    ): boolean
-
-    validateTag(
-      tagName: 'enum' | 'type' | 'returns' | 'yields',
-      ast: Ast,
-      value: any,
-      throwOnError?: boolean
-    ): Promise<Ast[]>
-
-    validateTypedef(
-      ast: Ast,
-      value: any,
-      throwOnError?: boolean
-    ) : ValidationError | Error
-  }
-
-  export class DocSchema {
-    approves(
-      value: any
-    ): boolean
-
-    check(
-      value: any
-    ): DocSchemaCheckResult
-
-    /**
-     * @template T
-     * @param {T} value Input value
-     * @returns {T} The input value, if it's valid
-     * @throws {ValidationError}
-     */
-    validate<T>(value: T): T
-  }
-
-  /**
-   * @returns {DocSchema}
-   * @throws {Error}
-   */
-  export function docSchema(): DocSchema
-
-  export default docSchema
-}
+export default docSchema
+export {
+  docSchema,
+  DocSchema,
+  DocSchemaParser,
+  DocSchemaValidator,
+  ValidationError
+} from './types.d.ts'
